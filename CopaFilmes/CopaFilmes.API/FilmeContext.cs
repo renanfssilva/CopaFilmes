0248b3a85d18358d832dec22c0a560adcd585f38
Renan Fernandes Silva <renanfssilva@gmail.com>
2020-01-23 00:25:41 -0300

    Configurando novo projeto
    Criando os projetos do server e client na solução.

---
+++ CopaFilmes/CopaFilmes.API/FilmeContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CopaFilmes.API.Models;
+
+namespace CopaFilmes.API
+{
+    public class FilmeContext : DbContext
+    {
+        public FilmeContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Filme> Filmes { get; set; }
+    }
+}
